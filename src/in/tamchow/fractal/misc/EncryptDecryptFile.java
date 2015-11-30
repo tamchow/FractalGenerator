9648643cc817490ce2a0c663da8ca3689f6d2e61
tamchow <chowtam8991@gmail.com>
2015-11-30 14:29:52 +0530

    Miscellaneous class for doing RC4 encryption of files

---
+++ src/in/tamchow/fractal/misc/EncryptDecryptFile.java
@@ -0,0 +1,46 @@
+package in.tamchow.fractal.misc;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Does file encryption/decryption using the RC4 class
+ */
+public class EncryptDecryptFile {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            //throw new IllegalArgumentException("No File Specified");
+            args = new String[1];
+            args[0] = "File.txt";
+        }
+        File input = new File(args[0]);
+        if (!input.exists()) {
+            throw new FileNotFoundException("Specified file does not exist");
+        }
+        File output;
+        if (input.getName().contains("_encrypted")) {
+            output = new File(input.getCanonicalPath().substring(0, input.getCanonicalPath().lastIndexOf('_')));
+            if (output.exists()) {
+                output.delete();
+            }
+        } else {
+            output = new File(input.getCanonicalPath() + "_encrypted");
+        }
+        DataInputStream dis = new DataInputStream(new FileInputStream(input));
+        ArrayList<Byte> intermediate = new ArrayList<>();
+        while (dis.available() > 0) {
+            intermediate.add(dis.readByte());
+        }
+        dis.close();
+        byte[] toprocess = new byte[intermediate.size()];
+        for (int i = 0; i < toprocess.length; i++) {
+            toprocess[i] = intermediate.get(i);
+        }
+        RC4 manip = new RC4();
+        byte[] processed = manip.process(toprocess);
+        DataOutputStream dos = new DataOutputStream(new FileOutputStream(output));
+        dos.write(processed, 0, processed.length);
+        dos.flush();
+        dos.close();
+    }
+}
