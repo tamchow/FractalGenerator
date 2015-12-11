9230fc0b33dc61fe7b9d148455ccd55311f5b0e2
tamchow <chowtam8991@gmail.com>
2015-12-11 22:51:24 +0530

    Final update before next major release

--- src/in/tamchow/fractal/math/FixedStack.java
+++ src/in/tamchow/fractal/math/FixedStack.java
@@ -1,18 +1,27 @@
 package in.tamchow.fractal.math;
 import in.tamchow.fractal.math.complex.Complex;
+
+import java.io.Serializable;
+import java.util.NoSuchElementException;
 /**
  * Implements a fixed-size stack of <code>Complex</code> objects
  */
-public class FixedStack {
+public class FixedStack implements Serializable {
     Complex[] elements;
     int top;
     public FixedStack(int capacity) {elements = new Complex[capacity]; resetTop();}
     public void resetTop() {top = elements.length;}
-    public void push(Complex value) {elements[--top] = value;}
-    public Complex pop() {return elements[top++];}
-    public Complex peek() {return elements[top];}
-    public boolean isEmpty() {return (top == elements.length);}
+    public void push(Complex value) {
+        if (isFull()) throw new IndexOutOfBoundsException("Overflow Exception"); elements[--top] = value;
+    }
     public boolean isFull() {return (top == 0);}
+    public Complex pop() {
+        if (isEmpty()) throw new NoSuchElementException("Underflow Exception"); return elements[top++];
+    }
+    public boolean isEmpty() {return (top == elements.length);}
+    public Complex peek() {
+        if (isEmpty()) throw new NoSuchElementException("Underflow Exception"); return elements[top];
+    }
     public int size() {return elements.length - top;}
     public void clear() {int capacity = elements.length; elements = new Complex[capacity]; resetTop();}
 }
