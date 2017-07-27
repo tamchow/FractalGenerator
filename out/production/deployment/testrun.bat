2a1309cf97c13d2b85c7d6f6bc0b516f79dd1e9e
Tamoghna Chowdhury <chowtam8991@gmail.com>
2017-07-27 15:05:44 +0530

    Update Readme and License, Code refactoring, New formulae

--- out/production/deployment/testrun.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -server -XX:FreqInlineSize=1000 -XX:CompileThreshold=1500 -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:+UseStringCache -XX:+UseParallelOldGC -XX:AllocatePrefetchStyle=2 -XX:+OptimizeStringConcat -Xms1536m -Xmx2048m -XX:PermSize=128m -XX:MaxPermSize=1024m -jar FractalGenerator.jar -t 2 4
-pause
--- out/production/deployment/testrun_debug.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-java -server -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintClassHistogram -XX:+PrintTenuringDistribution -XX:+PrintGCApplicationStoppedTime -XX:+PrintCompilation -XX:+UnlockDiagnosticVMOptions -XX:+PrintInlining -XX:FreqInlineSize=1000 -XX:CompileThreshold=1500 -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:+UseStringCache -XX:+UseParallelOldGC -XX:AllocatePrefetchStyle=2 -XX:+OptimizeStringConcat -Xms1536m -Xmx2048m -XX:PermSize=128m -XX:MaxPermSize=1024m -jar FractalGenerator.jar -t 2 4 > run.txt
-
-pause
